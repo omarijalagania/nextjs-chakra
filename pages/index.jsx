cf45e353acc59bc96684e3a655ffd72f9e02808f
Irakli <i.botchori@gmail.com>
2022-07-06 21:31:10 +0400

    style: change home page with sign-in page

---
+++ pages/index.jsx
@@ -0,0 +1,78 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  Stack,
+  useBreakpointValue,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import * as React from "react";
+import Link from "next/link";
+import { PasswordField } from "../components/PasswordField";
+
+export default function Home() {
+  return (
+    <Container
+      maxW="lg"
+      py={{
+        base: "12",
+        md: "24",
+      }}
+      px={{
+        base: "0",
+        sm: "8",
+      }}
+    >
+      <Stack spacing="8">
+        <Box
+          py={{
+            base: "0",
+            sm: "8",
+          }}
+          px={{
+            base: "4",
+            sm: "10",
+          }}
+          bg={useBreakpointValue({
+            base: "transparent",
+            sm: "bg-surface",
+          })}
+          boxShadow={{
+            base: "none",
+            sm: useColorModeValue("md", "md-dark"),
+          }}
+          borderRadius={{
+            base: "none",
+            sm: "xl",
+          }}
+        >
+          <Stack spacing="6">
+            <Stack spacing="5">
+              <FormControl>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input id="email" type="email" />
+              </FormControl>
+              <PasswordField />
+            </Stack>
+            <HStack justify="space-between">
+              <Checkbox defaultChecked>Remember me</Checkbox>
+              <Button variant="link" colorScheme="blue" size="sm">
+                Forgot password?
+              </Button>
+            </HStack>
+            <Stack spacing="6">
+              <Link href="/dashboard">
+                <Button variant="primary">Sign in</Button>
+              </Link>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
+  );
+}
